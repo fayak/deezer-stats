71c72e5a2bd5e941c0df7a9c9157210d5dd3e421
Zarak <cyril@fayak.com>
2020-11-05 20:51:19 +0100

    WIP: forgotten hits
    Signed-off-by: Zarak <cyril@fayak.com>

--- main.py
+++ main.py
@@ -8,64 +8,22 @@ import sys
 import argparse
 import utils
 
-associations = {
-        "title": "Song Title",
-        "artist": "Artist",
-        "ISRC": "ISRC",
-        "album": "Album Title",
-        "IP": "IP Address",
-        "list_time": "Listening Time",
-        "date": "Date",
-}
-keys = associations.keys()
 args = {}
-
-# datetime.strptime(line[DATE], "%Y-%m-%d %H:%M:%S.000")
-
-class Track(dict):
-    def __repr__(self):
-        return f"{self['title']} - {self['artist']}"
-
-    def __init__(self, track):
-        super().__init__({**track, "nb": 0, "list_time_sum": 0})
-        self.__dict__ = self
-        self += track
-
-    def __add__(self, other):
-        self.nb += 1
-        self.list_time_sum += other.list_time
-        return self
-
-class Catalog():
-    def __init__(self, tracks=None):
-        self.tracks = {}
-        if tracks is not None:
-            for track in tracks:
-                self += track
-
-    def __add__(self, other):
-        if other.ISRC in self.tracks:
-            self.tracks[other.ISRC] += other
-        else:
-            self.tracks[other.ISRC] = Track(other)
-        return self
-
-    def __getitem__(self, key):
-        return self.tracks[key.ISRC]
-
-    def search(self, q):
-        r = []
-        q = q.upper()
-        for _, track in self.tracks.items():
-            if q in str(track.title).upper():
-                r.append(track)
-        return r
 
 class ListTrack(dict):
     def __repr__(self):
         return f"[{self.date}] {str(timedelta(seconds=self.list_time))} - {self['title']} - {self['artist']}"
 
     def __init__(self, line):
+        associations = {
+                "title": "Song Title",
+                "artist": "Artist",
+                "ISRC": "ISRC",
+                "album": "Album Title",
+                "IP": "IP Address",
+                "list_time": "Listening Time",
+                "date": "Date",
+        }
         tmp_dict = {}
         for k, v in associations.items():
             if k == "list_time":
@@ -96,6 +54,52 @@ class ListTrack(dict):
         r['title'] = self.title
         return r
 
+class Track(dict):
+    def __repr__(self):
+        return f"{self['title']} - {self['artist']}"
+
+    def __init__(self, track):
+        track_obj_attr = ["ISRC", "artist", "title", "album"]
+        track_obj = {k: track[k] for k in track_obj_attr}
+        super().__init__({**track_obj, "nb": 0, "list_time_sum": 0})
+        self.__dict__ = self
+        self.dates = []
+        self += track
+
+    def __add__(self, other):
+        self.nb += 1
+        self.list_time_sum += other.list_time
+        self.dates.append(other.date)
+        return self
+
+    def __eq__(self, other):
+        return self.ISRC == other.ISRC
+
+class Catalog():
+    def __init__(self, tracks=None):
+        self.tracks = {}
+        if tracks is not None:
+            for track in tracks:
+                self += track
+
+    def __add__(self, other):
+        if other.ISRC in self.tracks:
+            self.tracks[other.ISRC] += other
+        else:
+            self.tracks[other.ISRC] = Track(other)
+        return self
+
+    def __getitem__(self, key):
+        return self.tracks[key.ISRC]
+
+    def search(self, q):
+        r = []
+        q = q.upper()
+        for _, track in self.tracks.items():
+            if q in str(track.title).upper():
+                r.append(track)
+        return r
+
 def parse_file(path, tracks, catalog):
     assert isinstance(tracks, list)
 
@@ -115,16 +119,32 @@ def config():
     actions.add_argument("--top-track", action="store_true", default=False, help="List the top tracks")
     actions.add_argument("--top-track-by-time", action="store_true", default=False, help="List the top tracks by listening time")
     actions.add_argument("--top", type=int, default=25, help="The top size")
-    actions.add_argument("--min", type=int, default=100, help="All songs listened minimun x times")
+    actions.add_argument("--min", type=int, default=None, help="All songs listened minimun x times")
+    actions.add_argument("--forgotten-hits", action="store_true", default=False)
+    actions.add_argument("--forgotten-hits-start", type=datetime.fromisoformat, default=None)
+    actions.add_argument("--forgotten-hits-end", type=datetime.fromisoformat, default=None)
+    actions.add_argument("--forgotten-hits-top", type=int, default=100)
+    actions.add_argument("--forgotten-hits-bucket-size", type=int, default=3)
+
+    filters = parser.add_argument_group('Filters')
+    filters.add_argument("--date-before", type=datetime.fromisoformat, default=None, help="Filter out tracks listened after this date")
+    filters.add_argument("--date-after", type=datetime.fromisoformat, default=None, help="Filter out tracks listened before this date")
 
     parser.add_argument("files", nargs="+")
     args = parser.parse_args()
+
     assert args.valid_algo in utils.validation_algorithms
-    actions = ["top_track", "top_track_by_time", "min"]
+    actions = ["top_track", "top_track_by_time", "min", "forgotten_hits"]
+    filters = ["date_before", "date_after"]
+
     args.actions = []
     for action in actions:
-        if getattr(args, action):
+        if getattr(args, action) is not None and getattr(args, action):
             args.actions.append(action)
+    args.filters = []
+    for filter in filters:
+        if getattr(args, filter) is not None:
+            args.filters.append(filter)
     return args
 
 def main():
@@ -138,6 +158,10 @@ def main():
 
     if len(args.actions) > 0:
         sub_tracks = [x for x in tracks if x.is_valid()]
+
+        for filter in args.filters:
+            sub_tracks = getattr(utils, filter)(sub_tracks, args=args)
+
         sub_catalog = Catalog(sub_tracks)
 
     for action in args.actions:
@@ -148,4 +172,5 @@ def main():
         es.reset_index()
         es.bulk(tracks)
 
-main()
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from datetime import timedelta
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 # VALIDATION ALGORITHMS
 
@@ -16,27 +18,90 @@ validation_algorithms = {
     "all": va_all,
     }
 
+# FILTERS
+def _date(tracks, method, **kwargs):
+    def _worker(tracks, method, date):
+        r = []
+        for track in tracks:
+            if getattr(track.date, method)(date):
+                r.append(track)
+        return r
+
+    if (date := kwargs.get("date", None)) is not None:
+        return _worker(tracks, method, date)
+    elif method == "__gt__":
+        return _worker(tracks, method, kwargs['args'].date_after)
+    else:
+        return _worker(tracks, method, kwargs['args'].date_before)
+
+def date_after(tracks, *args, **kwargs):
+    return _date(tracks, "__gt__", **kwargs)
+
+def date_before(tracks, *args, **kwargs):
+    return _date(tracks, "__lt__", **kwargs)
+
 # ACTIONS
 
-def print_tracks(tracks, title, args):
+def print_tracks(tracks, title, top):
     print(title)
-    for item,_ in zip(tracks, range(1, args.top + 1)):
-        print(f"[{_:02}] {item[1].nb} ({str(timedelta(seconds=item[1].list_time_sum))}): {item[1]}")
+    for item,_ in zip(tracks, range(1, top + 1)):
+        print(f"[{_:4}] {item[1].nb:3} ({str(timedelta(seconds=item[1].list_time_sum))}): {item[1]}")
+
+def _top_tracks(tracks, catalog):
+    sorted_tracks = sorted(catalog.tracks.items(), key=lambda item: item[1].nb, reverse=True)
+    return sorted_tracks
 
 def top_track(tracks, catalog, args):
-    sorted_tracks = sorted(catalog.tracks.items(), key=lambda item: item[1].nb, reverse=True)
-    print_tracks(sorted_tracks, "Top most listened track", args)
+    sorted_tracks = _top_tracks(tracks, catalog)
+    print_tracks(sorted_tracks, "Top most listened track", args.top)
 
 def top_track_by_time(tracks, catalog, args):
     sorted_tracks = sorted(catalog.tracks.items(), key=lambda item: item[1].list_time_sum, reverse=True)
-    print_tracks(sorted_tracks, "Top most listened track by listening-time", args)
+    print_tracks(sorted_tracks, "Top most listened track by listening-time", args.top)
 
 def min(tracks, catalog, args):
     sorted_tracks = sorted(catalog.tracks.items(), key=lambda item: item[1].nb, reverse=True)
     i = 1
+    print(f"Top tracks listened minimum {args.min} times")
     for iscr, track in sorted_tracks:
         if track.nb >= args.min:
-            print(f"[{i:02}] {track.nb} ({str(timedelta(seconds=track.list_time_sum))}): {track}")
+            print(f"[{i:4}] {track.nb:3} ({str(timedelta(seconds=track.list_time_sum))}): {track}")
             i += 1
         else:
             break
+
+def forgotten_hits(tracks, catalog, args):
+    from main import Catalog
+
+    date = datetime.now()
+    if args.forgotten_hits_start is None:
+        start_date = date - relativedelta(years=1)
+    else:
+        start_date = args.forgotten_hits_start
+    if args.forgotten_hits_end is None:
+        end_date = date
+    else:
+        end_date = args.forgotten_hits_end
+
+    top_ref = [k for k, v in _top_tracks(tracks, catalog)[:args.forgotten_hits_top]]
+
+    forgotten_hits = []
+    forgotten_hits_meta = []
+
+    while start_date < end_date:
+        bucket = date_after(date_before(tracks, date=(start_date + relativedelta(months=1))), date=start_date)
+        bucket_catalog = Catalog(bucket)
+        sorted_bucket = _top_tracks(bucket, bucket_catalog)
+        i = 0
+        for k, track in sorted_bucket:
+            if k in top_ref or k in forgotten_hits_meta:
+                continue
+            forgotten_hits.append((start_date, track))
+            forgotten_hits_meta.append(k)
+            i += 1
+            if i == args.forgotten_hits_bucket_size:
+                break
+
+        start_date += relativedelta(months=+1)
+    for date, track in forgotten_hits:
+        print(f"{date.date()} {track.nb:3} ({str(timedelta(seconds=track.list_time_sum))}): {track}")
